682ee0acf8605c5cd975396bda23c87ca4933e58
leojunioyuri <leojunioyuri@hotmail.com>
2024-09-23 04:12:50 -0300

    Feat: PWA APP

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+import nextPwa from 'next-pwa';
 
-export default nextConfig;
+/** @type {import('next').NextConfig} */
+const withPWA = nextPwa({
+  dest: 'public',
+  disable: process.env.NODE_ENV === 'development',
+});
+
+const nextConfig = {
+  // Outras configurações do Next.js aqui
+};
+
+export default withPWA(nextConfig);
--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,11 +1,23 @@
-'use client'
-import React from 'react';
-import { AppBar, Toolbar, Typography, Button } from '@mui/material';
+'use client';
+import React, { useState } from 'react';
+import { AppBar, Toolbar, Typography, Button, IconButton, Menu, MenuItem } from '@mui/material';
+import MenuIcon from '@mui/icons-material/Menu';
 import Link from 'next/link';
 import { useTheme } from '../utils/ThemeContext';
+import useMediaQuery from '@mui/material/useMediaQuery';
 
 const Navbar: React.FC = () => {
   const { darkMode, toggleTheme } = useTheme();
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+  const isMobile = useMediaQuery('(max-width:600px)');
+
+  const handleMenuOpen = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleMenuClose = () => {
+    setAnchorEl(null);
+  };
 
   return (
     <AppBar position="static">
@@ -13,19 +25,49 @@ const Navbar: React.FC = () => {
         <Typography variant="h6" sx={{ flexGrow: 1 }}>
           Goal Invest
         </Typography>
-        <Button component={Link} href="/" color="inherit" sx={{ marginRight: 2 }}>
-          Investimentos
-        </Button>
-        <Button component={Link} href="/faq" color="inherit" sx={{ marginRight: 2 }}>
-          FAQ
-        </Button>
-        <Button
-        variant="outlined"
-        color="inherit"
-        onClick={toggleTheme}
-      >
-        {darkMode ? 'Tema Claro' : 'Tema Escuro'}
-      </Button>
+
+        {isMobile ? (
+          <>
+            <Button
+              variant="outlined"
+              color="inherit"
+              onClick={toggleTheme}
+              sx={{ mr: 3, mb: 0.5 }}
+            >
+              {darkMode ? 'Tema Claro' : 'Tema Escuro'}
+            </Button>
+            <IconButton
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+              sx={{ mr: 1, mb: 0.5 }}
+              onClick={handleMenuOpen}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Menu anchorEl={anchorEl} open={Boolean(anchorEl)} onClose={handleMenuClose}>
+              <MenuItem onClick={handleMenuClose} component={Link} href="/">
+                Investimentos
+              </MenuItem>
+              <MenuItem onClick={handleMenuClose} component={Link} href="/faq">
+                FAQ
+              </MenuItem>
+            </Menu>
+          </>
+        ) : (
+          <>
+            {/* Desktop */}
+            <Button component={Link} href="/" color="inherit" sx={{ marginRight: 2 }}>
+              Investimentos
+            </Button>
+            <Button component={Link} href="/faq" color="inherit" sx={{ marginRight: 2 }}>
+              FAQ
+            </Button>
+            <Button variant="outlined" color="inherit" onClick={toggleTheme}>
+              {darkMode ? 'Tema Claro' : 'Tema Escuro'}
+            </Button>
+          </>
+        )}
       </Toolbar>
     </AppBar>
   );
